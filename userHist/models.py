d9446a4da7cb18bd188034b8ca40ac0e9857a888
aduggie <aidenduggan@gmail.com>
2022-03-20 17:18:46 -0500

    UserHistory update
    progression on user history and added user history button on taskbar

--- userHist/models.py
+++ userHist/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from account.models import Account
 
 # Create your models here.
+
+
+
+
+
+class Orders(models.Model):
+    user_name = models.CharField(max_length=200, null = True, blank = True)
+    order_ID = models.IntegerField(default=0, null=True, blank=True)
+    order_Date = models.DateTimeField(auto_now_add=False, auto_now=False, null = True)
+
+    def __str__(self):
+        return str(self.id)
+
+class OrderHistory(models.Model):
+    user_name = models.CharField(max_length=200, null = True, blank = True)
+    order_ID = models.IntegerField(default=0, null=True, blank=True)
+    order_Date = models.DateTimeField(auto_now_add=False, auto_now=False, null = True)
+
+
--- userHist/views.py
+++ userHist/views.py
@@ -5,5 +5,27 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
+
+from itertools import product
+from django.shortcuts import render
+from django.template import context
+
+from account.models import Account
+from store.models import Form, Formitems, Product
+
+# Create your views here.
+
+
+
 def userHistPage(request):
-    return render(request, 'userHist.html')
+    user = request.user
+    products = Product.objects.all()
+    accounts = Account.objects.all()
+    formitem = Formitems.objects.all()
+    form = Form.objects.all()
+    context = {'products':products,'account':accounts,'form': form, 'formitems':formitem}
+
+    return render(request, 'userHist.html', context)
+    
+
+
