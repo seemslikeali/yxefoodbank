654f7df023137fea3853c95d5c7bd79753fedb17
ginsan11 <thezerodude2@gmail.com>
2022-03-21 02:22:54 -0600

    Cart system (content render)
    Cart content now being rendered and displayed
    Still need to make remove/add and item count

--- store/models.py
+++ store/models.py
@@ -3,6 +3,14 @@ from account.models import Account
 
 # Create your models here.
 
+class Customer(models.Model):
+    user = models.OneToOneField(Account, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+
+
+    def __str__(self):
+        return self.name
 
 class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
--- store/views.py
+++ store/views.py
@@ -2,6 +2,8 @@ from itertools import product
 from django.shortcuts import render
 from django.template import context
 from .models import Product
+from .models import *
+
 
 # Create your views here.
 
@@ -9,6 +11,30 @@ from .models import Product
 def store(request):
     products = Product.objects.all()
     context = {'products':products}
+    customer = request.user
+    form = Form.objects.all().filter(customer=customer).filter(complete=False)
+    for x in form:
+        items = x.formitems_set.all()
+    context = {'products':products, 'items': items}
     return render(request, 'store.html', context)
 
+def cart(request):
+    print('----------------------------------->')
+    print(type(request.user))
+    print(request.user)
+    user = request.user
+    form = Form.objects.all().filter(user=user).filter(complete=False)
+    for x in form:
+        items = x.formitems_set.all()
+        print(x.transaction_id)
+        for item in items:
+            print(item.quantity)
+    #items = form.formitem_set.all()
+    context = {'items': items}
+    return render(request, 'cart.html', context)
 
+
+# def cart(request):
+#     customer = request.user.customer
+#     forms, created = form.objects.get_or_create()
+#     items = form.orderitem_set.all()
