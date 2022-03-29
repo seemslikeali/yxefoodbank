696ef36cdecf60149089779749556b8af700bad6
aduggie <aidenduggan@gmail.com>
2022-03-28 22:27:46 -0500

    Commenting

--- userHist/views.py
+++ userHist/views.py
@@ -19,17 +19,22 @@ from store.models import Customer, Form, Formitems, Product
 
 def userHistPage(request):
     user = request.user
+
+    # Creation of variables to call for each model
     products = Product.objects.all()
     accounts = Account.objects.all()
-    form = Form.objects.all().filter(customer=user).filter(complete=True)
     formitem = Formitems.objects.all()
+
+
+    form = Form.objects.all().filter(customer=user).filter(complete=True) # Specified user verification check
     context = {'products':products,'account':accounts,'form': form, 'formitems':formitem}
 
+    # Deletion of a specified form from userHist
     if (request.method == 'POST') and ('delete' in request.POST):
         item = request.POST.get('delete')
-        for x in form.filter(transaction_id = item):
-            x.delete()
-            return redirect('userHist')
+        for x in form.filter(transaction_id = item):    # Filters through items by specified transaction_id
+            x.delete()  # Deletes form model values via specified transaction_id from previous line
+            return redirect('userHist') # Refreshes website with updated changes
 
             
 
